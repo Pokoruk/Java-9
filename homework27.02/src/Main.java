bc31d2e3734a702012201547b2c894aa6ce05f67
Pokoruk <pokorukvlada@gmail.com>
2019-03-05 15:21:34 +0200

    Hw_4.03

---
+++ homework27.02/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a= 0;
+        Scanner b = new Scanner(System.in);
+        System.out.println("Enter number... ");
+        int c = b.nextInt();
+        int d = c; a= 0;
+        for (int e = 0; e < 1; e++) {
+            int f = c / 1;
+            int g = c/c;
+            System.out.println(f);
+            System.out.println(g);
+        }
+        System.out.println();
+
+
+    }
+}
